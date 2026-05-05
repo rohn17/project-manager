24ea720e1ab8a9c33ff1ca5b8b46220579fe09a7
rohn17 <rohanchoudhary.us17@gamil.com>
2026-05-05 15:22:33 +0530

    port req

--- backend/server.js
+++ backend/server.js
@@ -82,7 +82,7 @@ mongoose
 // ======================
 // Server Start
 // ======================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
